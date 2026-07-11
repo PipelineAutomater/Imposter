fc052aab857bd835837c1832fd9c8626e49b56ec
PipelineAutomater <TheGreatestOfAllTime1995@gmail.com>
2026-07-11 08:29:42 -0700

    Update App.tsx

--- imposter/src/App.tsx
+++ imposter/src/App.tsx
@@ -1,11 +1,8 @@
 import { type JSX } from 'react'
-import text from './assets/Categories.txt'
+import text from './assets/Categories.txt?raw'
 import './App.css'
 function App() {
-  return (<>
-    <DisplayPlayersBoxes />
-    <AddPlayer />
-  </>);
+  return (<DisplayWord/>);
 }
 
 function AddPlayer() {
@@ -13,17 +10,21 @@ function AddPlayer() {
 }
 
 function DisplayPlayersBoxes() {
-  const lines:string[] = text.split(/\r\n/);
-  const categories: string[] = lines.filter((line) => line.startsWith("Category:"));
-  const category:string = categories[Math.random()*categories.length];
-  const index = lines.indexOf(category);
-  const word:string = lines.slice(index + 1, index + 1000)[Math.random()*1000];
   const players:string[] = ["John", "Jack", "Jill"];
   const divs: JSX.Element[] = [];
   for (let i:number = 0; i < players.length; i++) {
     divs.push(<div className = "player-box">{players[i]}</div>);
   }
   return <div id = "player-box-container">{divs}</div>;
+}
+
+function DisplayWord() {
+  const lines:string[] = text.split(/\r\n/).filter(line => line !== "" && line !== "\r\n" && line !== "\n" && line !== "\r");
+  const categories: string[] = lines.filter((line) => line.startsWith("Category:"));
+  const category:string = categories[Math.floor(Math.random()*categories.length)];
+  const index = lines.indexOf(category);
+  const word:string = lines.slice(index + 1, index + 1000)[Math.floor(Math.random()*1000)];
+  return (<p>{category}<br/><br/>{word}</p>);
 }
 
 export default App
